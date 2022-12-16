c3a7fb82100e1400df9244f07d01b738e5ebeb14
errunlee <32530613+errunlee@users.noreply.github.com>
2022-12-16 14:27:03 +0545

    Final Desktop Version

--- script.js
+++ script.js
@@ -61,20 +61,37 @@ next.forEach(element=>{
     }
     else{
         console.log('no card chosen')
-        card_error.style.display='initial'
+        card_error.style.display='initial';
+        steps[2].classList.remove('active-step');
     }
    }
    if(currentStep===3){
     if(option1.checked===true ||option2.checked===true || option3.checked===true ){
+     document.querySelectorAll('.card_error')[0].style.display='none'
      step1.style.translate='-3000px';
      step2.style.translate='-2600px';
      step3.style.translate='-2200px';        
      step4.style.translate='-1800px'; 
     steps[2].classList.add('active-step');
-     currentStep++;      
+    setTimeout(()=>{
+      currentStep++;      
+    },100)
      step3summary(); 
      renderTotal.innerHTML=totalAmount;
     }
+    else{
+      document.querySelectorAll('.card_error')[0].style.display='initial';
+    steps[2].classList.remove('active-step');
+    }
+   }
+   if(currentStep===4){
+    steps[3].classList.add('active-step');
+    document.querySelector('.form-parent').innerHTML=`  <div class="form-fill p-3 me-5 mt-2 d-flex flex-column justify-content-center align-items-center" id="finishup">
+    <img class='mb-3' src="assets/images/icon-thank-you.svg">
+     <div class="mt-3" style="width:28rem;letter-spacing:2px;font-size:1.2rem">
+       <h3 class="text-center" style="font-family:'oswald bold'; color:rgb(9, 9, 80)">Thank you!</h3><h5 class='text-center' style="color:rgb(141, 131, 131);font-family: 'oswald'; font-size:15px; letter-spacing:0.5px;">Thanks for confirming your subscription! We hope you have fun using our platform. If you ever need support, please feel free to email us at support@loremgaming.ccom</h5>
+       </div>
+ </div> `
    }
 })
 })
@@ -123,6 +140,40 @@ function removePrevOpt(){
     })
 }
 
+//switch year or month
+let choosePackage=document.querySelector('#switch')
+choosePackage.addEventListener('click',()=>{
+  let priceItems=document.querySelectorAll('.mOrY')
+  let yearormonth=document.querySelectorAll('.yearormonth')
+  if(choosePackage.checked===true){
+    priceItems.forEach((element=>{
+      element.innerHTML=eval(element.innerHTML*10)
+    }))
+    yearormonth.forEach((element=>{
+      element.innerHTML='/yr'
+    }))
+    totalSum.innerHTML='Total (per year)'
+    finalmory.innerHTML='/yr'
+  }
+  else{
+    priceItems.forEach((element=>{
+      element.innerHTML=eval(element.innerHTML/10)
+    }))
+    yearormonth.forEach((element=>{
+      element.innerHTML='/mo'
+    }))
+    totalSum.innerHTML='Total (per month)'
+    finalmory.innerHTML='/mo'
+  }
+})
+
+let step3Selection=document.querySelectorAll('.bg-class')
+step3Selection.forEach((element=>{
+  element.addEventListener('click',()=>{
+    element.parentElement.parentElement.classList.toggle('new-bg')
+    console.log('clicked')
+  })
+}))
 function step3summary(){
     price=document.querySelector('.card-option p span').innerHTML;
     totalAmount=Number.parseInt(price);
